359a30d281336be975df846e7a986fcd4bea4826
xr <192319969@qq.com>
2025-12-22 16:24:09 +0800

    chore:format

--- src/include/Krpcapplication.h
+++ src/include/Krpcapplication.h
@@ -2,20 +2,20 @@
 #define _Krpcapplication_H
 #include "Krpcconfig.h"
 #include "Krpcchannel.h" 
-#include  "Krpccontroller.h"
+#include "Krpccontroller.h"
 #include<mutex>
 //Krpc基础类，负责框架的一些初始化操作
 class KrpcApplication
 {
 public:
     static void Init(int argc,char **argv);
-    static KrpcApplication & GetInstance();
     static void deleteInstance();
-    static Krpcconfig& GetConfig();
+    static KrpcApplication& GetInstance();
+    static Krpcconfig&      GetConfig();
 private:
-    static Krpcconfig m_config;
-    static KrpcApplication * m_application;//全局唯一单例访问对象
-    static std::mutex m_mutex;
+    static KrpcApplication*  m_application;//全局唯一单例访问对象
+    static Krpcconfig        m_config;
+    static std::mutex        m_mutex;
     KrpcApplication(){}
     ~KrpcApplication(){}
     KrpcApplication(const KrpcApplication&)=delete;
